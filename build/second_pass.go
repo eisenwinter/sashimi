68bba9af055a5de3ba0d85abbd9c6e579234ecb1
STARSHINE <jan.caspar@gmail.com>
2020-10-14 18:03:06 +0200

    we got a battle plan for second pass

--- build/second_pass.go
+++ build/second_pass.go
@@ -4,6 +4,21 @@ type secondPass struct {
 	*BaseSashimiListener
 }
 
+/*
+	Hookay basically here is what we need to do:
+	First - process the layout pages (layout_section), generate those fully and ready in some intermediate format
+	Second - walk the files, if we encounter a sashimi:layout directive we have the layout_section ready to inline the layout
+	For each file
+		- check if there are any command calls on entities that are not within a repeat scope, those pages will be duplicated for each entity
+		- once we know wheter a page is a single page or is going to be multiple pages we start walking the dom tree
+		- for each loop directive we extract the whole sccope as fragment
+		- for each link directive we look ahead for the next a tag (the next tag SHOULD be an a tag)
+		- for each display directive we try to resolve the entrity and replace any block content that currently exists with the resolved content
+
+	well this should be easy peasy lemon squeezy - but i guess its going to be hard hard lemon hard.
+
+
+*/
 func (s *secondPass) EnterLoopCall(ctx *LoopCallContext) {}
 
 func (s *secondPass) ExitLoopCall(ctx *LoopCallContext) {}
--- build/second_pass_test.go
+++ build/second_pass_test.go
@@ -17,7 +17,67 @@ func (m *mockNodeResolver) Resolve(nodeType string, predicate resolver.Predicate
 }
 
 var dummyProject = map[string]string{
-	"page.html":           "page",
-	"projects.html":       "projects",
-	"project.html":        "project",
-	"shared/_layout.html": "layout"}
+	"page.html": `<!-- sashimi:layout(main) -->
+	<!--
+	  sashimi:entity(page) of
+	   - title as "Pagetitle" is text
+	   - description as "Description" is text[:markdown]
+	-->
+	<div class="md-content">
+	  <!-- sashimi:display(page.description) -->
+	  This content wil be replaced with the page contents et al
+	</div>`,
+	"projects.html": `<!-- sashimi:layout(main) -->
+	<!-- sashimi:repeat(project) as p -->
+	<div class="project-item">
+		<h1><!-- sashimi:display(p.title) --> MyProject </h1>
+	   <!-- sashimi:link(p) -->
+	   <a href="#"></a>
+	</div>`,
+	"project.html": `<!-- 
+	sashimi:entity(project) of
+	 - title as "Project Title" is text
+	 - description as "Description" is text
+	 - image as "Main Image" is picture
+	 - starCount as "Star Count" is number
+	 - category as "Category" is oneOf("Archtiecture,"Misc.")
+	 - tags as "Tags" is manyOf("Cheap","Fast","Good")
+	 - gallery as "Gallery" is list of picture
+  -->
+  <div>
+	<h1><!-- sashimi:display(project.title) --> MyProject </h1>
+	<p>
+	  <!-- sashimi:display(project.description) --> 
+	  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, placeat praesentium, cum tempore in tenetur quo voluptate eum ipsa commodi laudantium assumenda vero modi itaque sunt vel qui est ut.
+	</p>
+  </div>`,
+	"shared/_layout.html": `<!doctype html>
+	<html class="no-js" lang="">
+	<head>
+	  <meta charset="utf-8">
+	  <title></title>
+	  <meta name="description" content="">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <meta property="og:title" content="">
+	  <meta property="og:type" content="">
+	  <meta property="og:url" content="">
+	  <meta property="og:image" content="">
+	</head>
+	<body>
+	  <nav>
+	   <ul>
+		<!-- sashimi:repeat(@pages) as e -->
+		<li>
+		   <!-- sashimi:link(e) -->
+		  <a href="#">Blah</a>
+		</li>
+	   </ul>
+	  </nav>
+	  <div class="main">
+	  </div>
+	  <div class="content">
+		<!-- sashimi:layout_section(main) -->
+	  </div>
+	</body>
+	</html>
+	`}
